615db4c6887266b45348bd59cbf2f330b7b0fadf
zpremium <zdravkoms99@gmail.com>
2021-07-10 18:08:26 +0200

    finished

--- public/script.js
+++ public/script.js
@@ -3,12 +3,14 @@ const colors = ["green", "red", "silver", "gray", "white", "brown", "purple", "l
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click",  () => {
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
+  color.style.color=colors[randomNumber];
+  btn.style.backgroundColor=colors[randomNumber];
 });
 
 function getRandomNumber() {
